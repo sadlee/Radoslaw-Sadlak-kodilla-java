0c2280d213ec58032ecbcce1341a96d733ece472
sadlee <sadlak.radoslaw@gmail.com>
2019-10-27 12:38:01 +0100

    First Commit

--- src/main/java/com/kodilla/kodillacourse/Kalkulator.java
+++ src/main/java/com/kodilla/kodillacourse/Kalkulator.java
@@ -5,15 +5,16 @@ class Calculator {
     public int addNumbers(int x, int y) {
         return x + y;
     }
+
     public int subtractNumbers(int x, int y) {
         return x - y;
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        int result = calculator.addNumbers(5,9);
-        int result1 = calculator.subtractNumbers(8,4);
+        int result = calculator.addNumbers(5, 9);
+        int result1 = calculator.subtractNumbers(8, 4);
 
         System.out.println(result);
         System.out.println(result1);
