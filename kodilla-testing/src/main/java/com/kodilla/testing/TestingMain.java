ee3cc3cd8969ab42874cf79b254e555f3c8dfa62
sadlee <sadlak.radoslaw@gmail.com>
2019-11-10 01:37:03 +0100

    third commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(5, 9);
+
+        if (result != 0) {
+            System.out.println("test 1 OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        int result1 = calculator.subtract(8, 4);
+
+        if (result1 != 0) {
+            System.out.println("test 2 OK");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
