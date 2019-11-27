3db0bc3ab1abed311e8194c07a9cbc24ec1254e0
sadlee <sadlak.radoslaw@gmail.com>
2019-11-27 22:12:13 +0100

    6.3

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,9 +1,6 @@
 package com.kodilla.testing.collection;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import java.util.ArrayList;
 import java.util.Random;
 
