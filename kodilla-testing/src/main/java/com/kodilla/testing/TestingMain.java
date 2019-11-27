5105417f7976b4a1e4e33c4475e9d9b6144a6229
Sadlak <radoslaw.sadlak@thomsonreuters.com>
2019-11-27 21:59:20 +0100

    6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,25 +1,8 @@
-package com.kodilla.testing;
+package com.kodilla.testing.forum;
 
-import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
 
-        int result = calculator.add(5, 9);
-
-        if (result != 0) {
-            System.out.println("test 1 OK");
-        } else {
-            System.out.println("Error");
-        }
-
-        int result1 = calculator.subtract(8, 4);
-
-        if (result1 != 0) {
-            System.out.println("test 2 OK");
-        } else {
-            System.out.println("Error");
-        }
     }
 }
