e8f4c102b03358c0b9b3cd51c6abd906c98ab8e1
sadlee <sadlak.radoslaw@gmail.com>
2019-12-20 17:38:39 +0100

    13.3

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,60 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    InvoiceDao invoiceDao;
+
+    @Test
+    public void testInvoiceDaoSave() {
+        //Given
+        Invoice invoice = new Invoice("012/12/2019");
+
+        Product bread = new Product("milk");
+        Product butter = new Product("nachos");
+        Product knife = new Product("bread");
+
+        Item itemMilk = new Item(bread, new BigDecimal(5), 2);
+        Item itemNachos = new Item(butter, new BigDecimal(9), 1);
+        Item itemBread = new Item(knife, new BigDecimal(250), 1);
+
+        itemMilk.setInvoice(invoice);
+        itemNachos.setInvoice(invoice);
+        itemBread.setInvoice(invoice);
+
+        List<Item> items = new ArrayList<>();
+        items.add(itemMilk);
+        items.add(itemNachos);
+        items.add(itemBread);
+
+        invoice.setItems(items);
+
+        //When
+        invoiceDao.save(invoice);
+        int invoiceId = invoice.getId();
+        int itemSize = invoice.getItems().size();
+
+        Invoice invoiceReadFromDB = invoiceDao.findById(invoiceId);
+
+        //Then
+        Assert.assertEquals(invoiceId, invoiceReadFromDB.getId());
+        Assert.assertEquals(3, itemSize);
+        Assert.assertEquals(itemSize, invoiceReadFromDB.getItems().size());
+    }
+}
