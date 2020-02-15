bcc56df86ca292bcd79c7b99c23d90c836f9250b
sadlee <sadlak.radoslaw@gmail.com>
2020-02-16 00:07:30 +0100

    23.2

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -1,13 +1,9 @@
 package com.kodilla.testing.forum.statistics;
 
 import org.junit.Test;
-import org.mockito.Mockito;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
 
 public class ForumStatisticsTestSuite {
 
