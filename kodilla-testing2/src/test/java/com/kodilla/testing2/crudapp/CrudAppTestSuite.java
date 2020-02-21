7e91bb1b267a845e95c06dbbad1f5016c60bd692
sadlee <sadlak.radoslaw@gmail.com>
2020-02-21 21:12:34 +0100

    24.1

--- kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
+++ kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
@@ -14,6 +14,7 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import static org.junit.Assert.assertTrue;
 
+
 public class CrudAppTestSuite {
     private static final String BASE_URL = "https://sadlee.github.io/";
     private WebDriver driver;
